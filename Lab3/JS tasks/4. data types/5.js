e1dba0df6456c8614b3c93efd5c850a89dd781d8
abdusss111 <abdusalamabdu29@gmail.com>
2024-02-14 21:39:24 +0600

    Lab3

--- Lab3/JS tasks/4. data types/5.js
+++ Lab3/JS tasks/4. data types/5.js
@@ -62,3 +62,54 @@ let messages = [
   
   readMap.set(messages[0], new Date(2017, 1, 1));
   // Date object we'll study later
+// 6
+function sumSalaries(salaries) {
+
+    let sum = 0;
+    for (let salary of Object.values(salaries)) {
+      sum += salary;
+    }
+  
+    return sum; // 650
+  }
+  
+  let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  
+  alert( sumSalaries(salaries) ); // 650
+
+  // 7
+  function count(obj) {
+    return Object.keys(obj).length;
+  }
+
+  // 8
+  let user = {
+    name: "John",
+    years: 30
+  };
+  
+  let {name, years: age, isAdmin = false} = user;
+  
+  alert( name ); // John
+  alert( age ); // 30
+  alert( isAdmin ); // false
+
+  // 9
+  function topSalary(salaries) {
+
+    let maxSalary = 0;
+    let maxName = null;
+  
+    for(const [name, salary] of Object.entries(salaries)) {
+      if (maxSalary < salary) {
+        maxSalary = salary;
+        maxName = name;
+      }
+    }
+  
+    return maxName;
+  }
